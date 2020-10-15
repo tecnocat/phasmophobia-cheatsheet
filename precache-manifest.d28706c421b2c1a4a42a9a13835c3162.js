eca3c895b38da886958b6638e7acc5b180cbe2a5
Alpha <alpha@pulsarcode.com>
2020-10-15 14:58:22 +0200

    FUCK GITHUB!!!

---
+++ precache-manifest.d28706c421b2c1a4a42a9a13835c3162.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f001433e3f4163666379f31c308099d0",
+    "url": "/phasmophobia-cheatsheet/index.html"
+  },
+  {
+    "revision": "6f38e6001756cc5b318f",
+    "url": "/phasmophobia-cheatsheet/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6ce996c918da3bfff55b",
+    "url": "/phasmophobia-cheatsheet/static/css/main.75b36bc9.chunk.css"
+  },
+  {
+    "revision": "6f38e6001756cc5b318f",
+    "url": "/phasmophobia-cheatsheet/static/js/2.b08e41b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/phasmophobia-cheatsheet/static/js/2.b08e41b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce996c918da3bfff55b",
+    "url": "/phasmophobia-cheatsheet/static/js/main.76f66000.chunk.js"
+  },
+  {
+    "revision": "006bdd8014f18be11f2a",
+    "url": "/phasmophobia-cheatsheet/static/js/runtime-main.35471a59.js"
+  },
+  {
+    "revision": "c97139a720471056ca4b7a0d4282869e",
+    "url": "/phasmophobia-cheatsheet/static/media/ghost.c97139a7.svg"
+  }
+]);
